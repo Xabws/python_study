aeb20697c677dc3b25e0f628665f07ae53e63a6d
白文帅 <xabws@163.com>
2018-11-06 17:54:11 +0800

    xxx

--- Python_Study2.py
+++ Python_Study2.py
@@ -26,38 +26,38 @@ number4 = 3e+26J
 
 message = "1234567"
 
-print (message[2]) # 截取字符串，留头不留尾
-print (message[2:])
-print (message * 2)
-print (message + "0000")
-print ("23" in message)
+print(message[2])  # 截取字符串，留头不留尾
+print(message[2:])
+print(message * 2)
+print(message + "0000")
+print("23" in message)
 
 # List:列表用[ ]标识。是python最通用的复合数据类型。:有序
 List = [000, "111", 1.05, 45343, 3e+26J]
-print (List)
+print(List)
 List[4] = "asdqw"
-print (List)
+print(List)
 
 # Tuple:只读列表
 myTuple = (000, "111", 1.05, 3434334)
-print (myTuple)
+print(myTuple)
 
 # Dictionary:字典是无序的对象集合。字典当中的元素是通过键来存取的，而不是通过偏移存取。'
 myDict = {"SIX": 6, "FIVE": 5, "FOUR": 4}  # 前面为键，后面为值
 
-print (myDict)
-print (myDict.keys())  # 所有的键
-print (myDict.values())  # all value
+print(myDict)
+print(myDict.keys())  # 所有的键
+print(myDict.values())  # all value
 
 dict = {}
 dict["one"] = "This is one"
 dict[2] = "This is two"
-print (dict["one"])  # 输出键为"one" 的值
-print (dict[2])  # 输出键为 2 的值
+print(dict["one"])  # 输出键为"one" 的值
+print(dict[2])  # 输出键为 2 的值
 
 # 运算符
-print (1 ** 3)  # **取幂：1的3次幂
-print (1 // 3 ) # //	取整除 :返回商的整数部分
+print(1 ** 3)  # **取幂：1的3次幂
+print(1 // 3)  # //	取整除 :返回商的整数部分
 
 # 逻辑运算符
 # and	布尔"与" - 如果x为False，x and y返回False，否则它返回y的计算值。	(a and b) 返回 true。----->&&a
@@ -66,10 +66,10 @@ print (1 // 3 ) # //	取整除 :返回商的整数部分
 
 x = True
 y = False
-print( x and y)
-print (x or y)
-print( not y)
-print ("--------")
+print(x and y)
+print(x or y)
+print(not y)
+print("--------")
 
 # 成员运算符
 # 运算符	描述	实例
@@ -78,47 +78,47 @@ print ("--------")
 a = 10
 b = 20
 list = [1, 2, 3, 4, 5];
-print (a in list)
-print (a not in list)
+print(a in list)
+print(a not in list)
 
 # 条件语句
 # Python程序语言指定任何非0和非空（null）值为true，0 或者 null为false。
 if 1 == 2:
-    print ("1==2")
+    print("1==2")
 elif 1 < 2:
-    print ("1<2")
+    print("1<2")
 else:
-    print ("1>2")
+    print("1>2")
 # 循环语句
 count = 0
 while (count < 9):
-    print ('The count is:', count)
+    print('The count is:', count)
     count = count + 1
-print ("Good bye!")
-#在 python 中，for … else 表示这样的意思，for 中的语句和普通的没有区别，else 中的语句会在循环正常执行完（即 for 不是通过 break 跳出而中断的）的情况下执行，
+print("Good bye!")
+# 在 python 中，for … else 表示这样的意思，for 中的语句和普通的没有区别，else 中的语句会在循环正常执行完（即 for 不是通过 break 跳出而中断的）的情况下执行，
 # while … else 也是一样。
 count = 0
 while count < 5:
-   print (count, " is  less than 5")
-   count = count + 1
+    print(count, " is  less than 5")
+    count = count + 1
 else:
-   print (count, " is not less than 5")
+    print(count, " is not less than 5")
 
-#for循环
+# for循环
 for letter in "Python":
-    print (letter)
+    print(letter)
 
-count =[1,2,3,4,5]
+count = [1, 2, 3, 4, 5]
 
 for number in count:
-    print (number)
+    print(number)
 
-numlist = [11,12,13,14,15,16]
-for number in range(len(numlist)):#range:范围，len：list的size
-    print (numlist[number])
-else:#在循环自然结束后执行
-    print ("end")
-print ("-----------------")
+numlist = [11, 12, 13, 14, 15, 16]
+for number in range(len(numlist)):  # range:范围，len：list的size
+    print(numlist[number])
+else:  # 在循环自然结束后执行
+    print("end")
+print("-----------------")
 List1 = ['zi', 'qiang', 'xue', 'tang']
 List2 = [1, 2]
 
@@ -128,13 +128,12 @@ for m in List1:
     for n in List2:
         new_list.append([m, n])
 
-print (new_list)
-print ("-----------------")
+print(new_list)
+print("-----------------")
 n = 20
 for i in range(1, n):
     for j in range(2, int(i ** 0.5)):
         if i % j == 0:
             break
     else:
-        print (i, '是素数')
-
+        print(i, '是素数')
