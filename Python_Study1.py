6787467b4105ad565a1dcfa94c45e8310e75c2ea
白文帅 <xabws@163.com>
2018-11-07 09:38:55 +0800

    xx

--- Python_Study1.py
+++ Python_Study1.py
@@ -55,30 +55,23 @@ print(word + word2 + word3)
 # Python可以在同一行中使用多条语句，语句之间使用分号(;)分割，以下是一个简单的实例：
 import sys;
 
-x = 'foo';
-sys.stdout.write(x + '\n')
+x = 'foo';sys.stdout.write(x + '\n')
 
 # 缩进相同的一组语句构成一个代码块，我们称之代码组。
 # 像if、while、def和class这样的复合语句，首行以关键字开始，以冒号( : )结束，该行之后的一行或多行代码构成代码组。
 # 我们将首行及后面的代码组称为一个子句(clause)。
 if 5 / 2 == 3:
-    print
-    "3"
+    print("3")
 elif 5 / 2 == 2:
-    print
-    '2'
+    print('2')
 else:
-    print
-    '1'
+    print('1')
 
 # 多个变量赋值：
 a, b, c = 1, 1.2, "hello"
-print
-a
-print
-b
-print
-c
+print(a)
+print(b)
+print(c)
 
 
 def xxx(a, b):
